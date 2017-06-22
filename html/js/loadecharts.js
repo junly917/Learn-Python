54aa734c053306be5d64f2feb6927a05ee07bb7e
junly_huang <junly_huang@sina.com>
2017-06-22 22:28:49 +0800

    transfer file

--- html/js/loadecharts.js
+++ html/js/loadecharts.js
@@ -1,17 +1,3 @@
 /**
  * Created by Junly on 2017/6/14.
  */
-function  loadhostinfo() {
-    var myChart = echarts.init(document.getElementById('main'));
-    console.log('123');
-    myChart.setOption({
-        series: [
-            {
-                name: '访问来源', type: 'pie', radius: '55%',
-                data: [
-                    {value: 235, name: '视频广告'}, {value: 274, name: '联盟广告'}, {value: 400, name: '搜索引擎'}
-                ]
-            }
-        ]
-    });
-}
