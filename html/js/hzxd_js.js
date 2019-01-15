2f838e07284e9c185ee71bad311b815931dca49c
junly_huang@sina.com <huangamw139>
2019-01-15 20:33:33 +0800

    new html

--- html/js/hzxd_js.js
+++ html/js/hzxd_js.js
@@ -1,11 +1,25 @@
 //菜单点击事件
-$(".item .menu").click(function () {
-    var all_list = $(this).parent().siblings();
-    all_list.children().eq(1).addClass('hide');
-    $(this).siblings().removeClass('hide');
+$(".m_menu").click(function () {
+    var all_list = $(this).parent().siblings().not("#ds");
+    // var q = all_list.children().children().eq(1)
+    // console.log(q)
+        // q.addClass('hide');
+    // $(this).siblings().removeClass('hide');
 })
 
 //登录事件
 $("#login").click(function () {
-    console.log($(#username,#password))
+    var u = $("#login-user").val()
+    var p = $("#login-password").val()
+    if (u.length == 0 ) {
+        $("#u-err").removeClass('hide');
+    }
+    if (p.length == 0 ) {
+         $("#p-err").removeClass('hide');
+    }
+    if((u=='admin') && (p=='admin')){
+        location.href="index.html"
+    }else {
+        $("#u-p-err").removeClass('hide');
+    }
 })
